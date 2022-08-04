93f2e87c53dc90f9bd9cb90ca49df7e905b94fb4
GorMax0 <102469814+GorMax0@users.noreply.github.com>
2022-08-05 00:05:57 +0300

    Work on spring joint

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 using Dreamteck.Splines;
 
-[RequireComponent(typeof(SplineFollower))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private BulletPool _bulletPool;
     [SerializeField] private ShotZone _shotZone;
+    [SerializeField] private Rigidbody _mover;
     [Header("Control Buttons")]
     [SerializeField] private MoverButton _buttonForward;
     [SerializeField] private MoverButton _buttonBackward;
@@ -31,13 +31,24 @@ public class PlayerController : MonoBehaviour
         _shotZone.Clicked -= Shoot;
     }
 
+    private void FixedUpdate()
+    {
+        _mover.velocity = new Vector3(0f, 0f, 1.5f);
+      //  _mover.rotation *= Quaternion.Euler(_x, 0f, 0f);
+    }
+
     private void Move(bool isClick, Spline.Direction direction)
     {
-        if (_follower.direction != direction)
-            _follower.direction = direction;
 
-        _follower.follow = isClick;       
+
     }
+    //private void Move(bool isClick, Spline.Direction direction)
+    //{
+    //    if (_follower.direction != direction)
+    //        _follower.direction = direction;
+
+    //    _follower.follow = isClick;       
+    //}
 
     private void Shoot(Vector3 target)
     {
