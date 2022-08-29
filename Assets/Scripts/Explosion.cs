c8cfe8987fc66d0d2043d5c14817beac640dea8d
GorMax0 <102469814+GorMax0@users.noreply.github.com>
2022-08-29 21:03:16 +0300

    Changed road model and explosion script

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -1,10 +1,9 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Explosion : MonoBehaviour
 {
     [SerializeField]private float _explosionPower;
-    [SerializeField]private Transform _parts;    
+     [SerializeField]private Transform _parts;    
 
     private bool _isExploded;
     private MeshRenderer _meshRenderer;
@@ -36,7 +35,8 @@ public class Explosion : MonoBehaviour
 
         foreach (var rigidbody in _rigidbodies)
         {
-            Vector3 force = (rigidbody.transform.position - origin).normalized * _explosionPower;
+            var randomForce = Random.Range(0.3f, 1.5f);
+            Vector3 force = (rigidbody.transform.position - origin).normalized * _explosionPower + new Vector3(0f,randomForce,randomForce);
             rigidbody.isKinematic = false;
             rigidbody.AddForce(force, ForceMode.VelocityChange);
         }
